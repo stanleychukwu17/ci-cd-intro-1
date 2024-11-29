1804fe442aec5043e0905cb627199d7eee533721
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-11-29 10:28:41 +0100

    fixed the error

--- web/tests/routes.test.js
+++ web/tests/routes.test.js
@@ -3,20 +3,19 @@ const app = require('../app')
 
 describe('Default Endpoint', () => {
   it('should return status 200', async () => {
-    const res = await request(app)
-      .get('/')
+    const res = await request(app).get('/')
     expect(res.statusCode).toEqual(200)
   })
 })
 
 describe('New Endpoint', () => {
   it('should return a static json response with status 201', async () => {
-    const res = await request(app)
-      .get('/ice-flakes')
+    const res = await request(app).get('/ice-flakes')
+
     expect(res.body).toEqual({
       resource: 'ice-flakes',
-      count: 205,
-      shape: 'circle'
+      count: 1005,
+      shape: 'rectangle'
     })
     expect(res.statusCode).toEqual(201)
   })
