c2453ba686d5946204712cbc29fbfe365353c970
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-12-24 07:18:41 +0100

    fixed the cidr_block

--- infra/instances/staging/main.tf
+++ infra/instances/staging/main.tf
@@ -14,12 +14,12 @@ provider "aws" {
 }
 
 resource "aws_vpc" "main" {
-  cidr_block = "237.84.2.178/16"
+  cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support = true
 
   tags = {
-    Name = "main VPC"
+    Name = "Ci-Cd VPC"
   }
   
 }
