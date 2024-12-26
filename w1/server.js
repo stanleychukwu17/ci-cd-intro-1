b2348d772a5aeddda9f03acf6926b4757708e37e
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-12-26 15:28:41 +0100

    using env port instead of hard coding the port

--- w1/server.js
+++ w1/server.js
@@ -1,5 +1,6 @@
-const app = require("./app")
-const port = 4000;
+require('dotenv').config();
+const app = require("./app");
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
