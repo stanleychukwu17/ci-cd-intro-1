bb615bdb22eaa09d50e2818e75967cde1b3d40d0
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-12-29 14:20:14 +0100

    added the variable key as a string

--- infra/staging/variables.tf
+++ infra/staging/variables.tf
@@ -15,7 +15,13 @@ variable "allowed_ip_address" {
   type        = list(string)
 }
 
+variable "public_key" {
+  description = "public key"
+  type        = string
+}
+
 variable "ssh_key_path" {
   description = "key pair path"
   type        = string
+  
 }
